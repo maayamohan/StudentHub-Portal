537169fa3ed70a94c636120b24a3d792ffa32253
Maaya Mohan <maayamohan21@gmail.com>
2025-12-01 19:31:01 +0530

    feat: Add studentcard to app component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
+import StudentCard from "./components/StudentCard";
+
 export default function App() {
   return (
     <div>
       <h1>StudentHub Portal</h1>
       <p>Manage Students Easily</p>
+      <br />
+      <StudentCard />
+      <StudentCard />
     </div>
   );
 }
