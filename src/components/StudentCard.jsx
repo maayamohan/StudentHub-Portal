5e8d94c163087687f1f55e2cd2d00fd0d327c2cc
Maaya Mohan <maayamohan21@gmail.com>
2025-12-12 20:11:24 +0530

    feat: Add prop with conditional rendering in studentcard

--- src/components/StudentCard.jsx
+++ src/components/StudentCard.jsx
@@ -1,7 +1,7 @@
 export default function StudentCard ({ name, dept }) {
     return (
         <div className="card">
-            <h3>{name}</h3>
+            <h3>{name} {scholarship && "🎓"}</h3>
             <p>Department: {dept}</p>
         </div>
     );
