a048281087dc3ecacc71cce7a3f234dfd5575c9c
Maaya Mohan <maayamohan21@gmail.com>
2025-12-12 20:46:08 +0530

    feat: Add click event handler in studentcard

--- src/components/StudentCard.jsx
+++ src/components/StudentCard.jsx
@@ -1,8 +1,14 @@
 export default function StudentCard ({ name, dept, scholarship }) {
+
+    function handleView() {
+        alert(`${name}'s details`)
+    }
+
     return (
         <div className="card">
             <h3>{name} {scholarship && "🎓"}</h3>
             <p>Department: {dept}</p>
+            <button onClick={handleView}>View Details</button>
         </div>
     );
 }
